46b60cfdc3940030db3eb5e250bd2643b24c1361
Cassey Lottman <clottman@ng.com>
2017-08-10 13:18:00 -0500

    console proj references net framework sharedLib

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharedLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace MultiTargetNetStandard
     {
         static void Main(string[] args)
         {
+
+            TestClass test = new TestClass();
+            test.SayHi();
+            Console.ReadLine();
         }
     }
 }
---
+++ SharedLib/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib
+{
+    public class TestClass
+    {
+        public void SayHi()
+        {
+            Console.WriteLine($"Hello from {nameof(TestClass)}!");
+        }
+    }
+}
